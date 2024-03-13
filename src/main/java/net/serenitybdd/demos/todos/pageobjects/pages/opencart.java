8eb29384039e21b37287f506305c91948d9cff62
candrademos <cinthya.andrademos@hotmail.com>
2024-03-12 22:10:47 -0500

    Cambios y ajustes

--- src/main/java/net/serenitybdd/demos/todos/pageobjects/pages/opencart.java
+++
@@ -1,5 +0,0 @@
-package net.serenitybdd.demos.todos.pageobjects.pages;
-import net.serenitybdd.core.pages.PageObject;
-import net.serenitybdd.annotations.DefaultUrl;
-@DefaultUrl("http://opencart.abstracta.us/")
-public class opencart extends PageObject {}
--- src/main/java/net/serenitybdd/demos/todos/screenplay/UI/demo/AggProductos.java
+++
@@ -1,7 +0,0 @@
-package net.serenitybdd.demos.todos.screenplay.UI.demo;
-import net.serenitybdd.screenplay.targets.Target;
-
-public class AggProductos {
-    public static final Target macBook = Target.the("Agregar primer producto MacBook").locatedBy("//span[@class='hidden-xs hidden-sm hidden-md'][contains(.,'Add to Cart')])[1]");
-    public static final Target ITEMS = Target.the("Agregar segundo producto iPhone").locatedBy("(//span[@class='hidden-xs hidden-sm hidden-md'][contains(.,'Add to Cart')])[2]");
-    }
